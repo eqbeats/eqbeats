69aae2eda312c53f08bb49fb0e11cfa4903ba807
eqbeats team <eqbeats@eqbeats.org>
2012-01-01 18:21:31 +0100

    add: stats miniprogram

---
+++ src/stats.cpp
@@ -0,0 +1,19 @@
+#include "db.h"
+#include <iostream>
+
+std::string show(const std::string &q){
+    DB::Result r = DB::query(q);
+    if(r.empty()) return std::string();
+    return r[0][0];
+}
+
+int main(int argc, char **argv){
+    DB::connect();
+    std::cout << show("SELECT count(*) FROM tracks") << " tracks ("
+              << show("SELECT count(*) FROM tracks WHERE visible='f'") << " hidden)" << std::endl
+              << show("SELECT count(*) FROM users") << " users" << std::endl
+              << show("SELECT count(*) FROM users WHERE EXISTS ( SELECT 1 FROM tracks WHERE user_id = users.id AND visible = 't' )") << " artists" << std::endl
+              << "Last user: " << show("SELECT name FROM users ORDER BY registration DESC LIMIT 1") << std::endl;
+    DB::close();
+    return 0;
+}
