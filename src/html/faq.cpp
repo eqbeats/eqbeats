f5e73b2758bac1d77339fcc777d4da856f8cd126
eqbeats team <eqbeats@eqbeats.org>
2012-01-16 21:24:43 +0100

    add who we are to FAQ

--- src/html/faq.cpp
+++ src/html/faq.cpp
@@ -12,6 +12,9 @@ std::string Html::faq(){
         "<div><h4>Why not [INSERT SITE HERE]?</h4>"
         "<p>Because we try to make the publishing and listening process as smooth as can be. No ads, no limits, no downsides.</p></div>"
 
+        "<div><h4>Who are you anyway?</h4>"
+        "<p>We're two developers: <a href=\"/user/1\">codl</a> and <a href=\"/user/2\">fmang</a>.</p></div>"
+
         "<div><h4>I've got a question/problem/idea!</h4>"
         "<p>What are you waiting for? Drop us a line at contact@eqbeats.org.</p></div>"
 
