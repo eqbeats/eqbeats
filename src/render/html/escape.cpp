60ce721b83221cc9b539040f1fde724b1d360b3b
eqbeats team <eqbeats@eqbeats.org>
2012-03-03 15:37:59 +0100

    regex-based format and strip

--- src/render/html/escape.cpp
+++ src/render/html/escape.cpp
@@ -1,7 +1,6 @@
 #include "escape.h"
 #include "../../number.h"
 #include <pcrecpp.h>
-#include <algorithm>
 
 using namespace std;
 using namespace Render;
@@ -27,40 +26,16 @@ string Html::escapeEmail(const string &email){
 
 string Html::format(string txt){
     txt = escape(txt);
-    string tmp;
-    for(string::const_iterator i=txt.begin(); i!=txt.end(); i++){
-        if(*i == '\n') tmp += "<br />";
-        else tmp += *i;
-    }
-    pcrecpp::RE("\\[b\\](.*?)\\[/b\\]").GlobalReplace("<b>\\1</b>", &tmp);
-    pcrecpp::RE("\\[i\\](.*?)\\[/i\\]").GlobalReplace("<i>\\1</i>", &tmp);
-    pcrecpp::RE("\\[u\\](.*?)\\[/u\\]").GlobalReplace("<u>\\1</u>", &tmp);
-    pcrecpp::RE("(http|ftp|https):\\/\\/[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?").GlobalReplace("<a href=\"\\0\" rel=\"nofollow\">\\0</a>", &tmp);
-    return tmp;
+    pcrecpp::RE("\r?\n").GlobalReplace("<br />", &txt);
+    pcrecpp::RE("\\[b\\](.*?)\\[/b\\]").GlobalReplace("<b>\\1</b>", &txt);
+    pcrecpp::RE("\\[i\\](.*?)\\[/i\\]").GlobalReplace("<i>\\1</i>", &txt);
+    pcrecpp::RE("\\[u\\](.*?)\\[/u\\]").GlobalReplace("<u>\\1</u>", &txt);
+    pcrecpp::RE("(http|ftp|https):\\/\\/[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?").GlobalReplace("<a href=\"\\0\" rel=\"nofollow\">\\0</a>", &txt);
+    return txt;
 }
 
-bool tag(string::const_iterator &beg, string::const_iterator &end){
-    beg = find(beg, end, '[');
-    string::const_iterator tagEnd = find(beg, end, ']');
-    if(tagEnd != end){
-        end = tagEnd+1;
-        return true;
-    }
-    return false;
-}
-
-string Html::strip(const string &str){
-    string::const_iterator beg = str.begin(), end = str.end();
-    // Opening tag
-    string::const_iterator open=beg, endOpen=end;
-    if(!tag(open, endOpen)) return str;
-    // Closing tag
-    string::const_iterator close=endOpen, endClose=end;
-    while(tag(close, endClose)){
-        if(string(close+2, endClose) == string(open+1, endOpen)) // Tag closed
-            return string(beg, open) + strip(string(endOpen, close)) + string(endClose, end);
-        close = endClose;
-        endClose = end;
-    }
-    return string(beg, endOpen) + strip(string(endOpen, end));
+string Html::strip(string txt){
+    pcrecpp::RE("\\.?[ \r?\n]*\n").GlobalReplace(". ", &txt);
+    pcrecpp::RE("\\[/?[uib]\\]").GlobalReplace("", &txt);
+    return txt;
 }
--- src/render/html/escape.h
+++ src/render/html/escape.h
@@ -10,7 +10,7 @@ std::string escape(const std::string &);
 std::string escapeEmail(const std::string &);
 
 std::string format(std::string);
-std::string strip(const std::string&);
+std::string strip(std::string);
 
 }
 }
