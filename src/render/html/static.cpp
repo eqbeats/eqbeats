5b49b9b2971ed788b508186b6ff72cee4b50a1be
eqbeats team <eqbeats@eqbeats.org>
2012-03-11 15:09:02 +0100

    legend: ownership
    users should only publish their own tracks

--- src/render/html/static.cpp
+++ src/render/html/static.cpp
@@ -6,7 +6,8 @@ using namespace Render;
 
 void Html::quickStart(){
     header("Thanks");
-    o << "<div class=\"message\">Now that you've registered you can upload tracks: just click your name at the top right corner of the page.</div>";
+    o << "<div class=\"message\">Now that you've registered you can upload your tracks and follow artists: just click your name at the top right corner of the page.</div>"
+         "<div class=\"message\">Don't submit any tracks but your own, though.</div>";
     footer();
 }
 
