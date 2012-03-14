afeee03178b17a25610dec1de043dfe28d46d22f
eqbeats team <eqbeats@eqbeats.org>
2012-03-14 18:27:16 +0100

    mail: better thread management

--- src/mail.cpp
+++ src/mail.cpp
@@ -9,7 +9,8 @@
 void sendMail(const char *to, const char *data){
     int fd[2];
     pipe(fd);
-    if(fork() == 0){
+    pid_t child = fork();
+    if(child == 0){
         close(fd[1]);
         close(0);
         dup(fd[0]);
@@ -18,18 +19,20 @@ void sendMail(const char *to, const char *data){
         freopen(logfile.c_str(),"a",stderr);
         execlp("sendmail", "sendmail", to, NULL);
     }
-    close(fd[0]);
-    time_t now = time(NULL);
-    struct tm *utc = gmtime(&now);
-    char buf[50];
-    strftime(buf, 50, "%d %b %Y %H:%M %z", utc);
-    std::string cnt = (std::string)
-        "Date: " + buf + "\n"
-        "To: <" + to + ">\n"
-        "Reply-To: EqBeats <eqbeats@eqbeats.org>\n"
-        "Content-Type: text/plain\n"
-        + data;
-    write(fd[1], cnt.c_str(), cnt.size());
-    close(fd[1]);
-    wait(NULL);
+    else if(child > 0){
+        close(fd[0]);
+        time_t now = time(NULL);
+        struct tm *utc = gmtime(&now);
+        char buf[50];
+        strftime(buf, 50, "%d %b %Y %H:%M %z", utc);
+        std::string cnt = (std::string)
+            "Date: " + buf + "\n"
+            "To: <" + to + ">\n"
+            "Reply-To: EqBeats <eqbeats@eqbeats.org>\n"
+            "Content-Type: text/plain\n"
+            + data;
+        write(fd[1], cnt.c_str(), cnt.size());
+        close(fd[1]);
+        waitpid(child, NULL, 0);
+    }
 }
