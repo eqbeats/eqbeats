c3213383a21d9259e846c5f3a4e55732aa317563
eqbeats team <eqbeats@eqbeats.org>
2012-04-17 12:12:21 +0200

    stats: playlist count

--- src/tools/stats.cpp
+++ src/tools/stats.cpp
@@ -11,6 +11,7 @@ int main(int argc, char **argv){
     DB::connect();
     std::cout << show("SELECT count(*) FROM tracks") << " tracks ("
               << show("SELECT count(*) FROM tracks WHERE visible='f'") << " hidden)" << std::endl
+              << show("SELECT count(*) FROM playlists") << " playlists" << std::endl
               << show("SELECT count(*) FROM users") << " users" << std::endl
               << show("SELECT count(*) FROM users WHERE EXISTS ( SELECT 1 FROM tracks WHERE user_id = users.id AND visible = 't' )") << " artists" << std::endl
               << "Last user: " << show("SELECT name FROM users ORDER BY registration DESC LIMIT 1") << std::endl;
