ee25e61415c04455af49fa0c7b69d85f998db76b
eqbeats team <eqbeats@eqbeats.org>
2012-04-19 11:12:58 +0200

    add --airable switch to dumptracks

--- src/tools/dumptracks.cpp
+++ src/tools/dumptracks.cpp
@@ -17,20 +17,21 @@ std::string escape(const std::string &str){
 int main(int argc, char **argv){
     DB::connect();
 
-    bool all = false, id=false;
+    bool all=false, id=false, airable=false;
     for(int i=1; i<argc; i++){
         if(strcmp(argv[i], "--all") == 0 || strcmp(argv[i], "-a") == 0) all = true;
+        if(strcmp(argv[i], "--airable") == 0) airable = true;
         if(strcmp(argv[i], "--id") == 0 || strcmp(argv[i], "-i") == 0) id = true;
     }
 
     if(id){
-        DB::Result r = DB::query( (std::string) "SELECT id FROM tracks" + (all?"":" WHERE visible='t'"));
+        DB::Result r = DB::query((std::string) "SELECT id FROM tracks" + (all?"":(" WHERE visible='t'" + (std::string) (airable?" AND airable='t'":""))) + " ORDER BY id ASC");
         for(DB::Result::const_iterator i=r.begin(); i!=r.end(); i++)
             std::cout << (*i)[0] << std::endl;
     }
 
     else {
-        DB::Result r = DB::query( (std::string) "SELECT tracks.user_id, users.name, tracks.id, tracks.title FROM tracks, users WHERE users.id = tracks.user_id " + (all?"":"AND visible='t' ") + "ORDER BY id ASC");
+        DB::Result r = DB::query((std::string) "SELECT tracks.user_id, users.name, tracks.id, tracks.title FROM tracks, users WHERE users.id = tracks.user_id " + (all?"":("AND visible='t' " + (std::string) (airable?"AND airable='t' ":""))) + "ORDER BY id ASC");
         for(DB::Result::const_iterator i=r.begin(); i!=r.end(); i++)
             std::cout << (*i)[0] << " " << escape((*i)[1]) << " - " << (*i)[2] << " " << (*i)[3] << std::endl;
     }
