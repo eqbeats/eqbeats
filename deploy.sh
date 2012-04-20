a52f7f684f55d513c2dd9d2f3a3ffb4e62395ae6
eqbeats team <eqbeats@eqbeats.org>
2012-04-20 09:32:51 +0200

    typos in API docs

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 cd /home/eqbeats/eqbeats
 make || return 1
-install -m775 eqbeats.fcgi failsafe hitsd.sh /srv/eqbeats/
+install -m775 eqbeats.fcgi failsafe hitsd.sh ytmgr.sh /srv/eqbeats/
 
 cd tools
 find . -maxdepth 1 -type f -executable -exec install -m775 '{}' /srv/eqbeats/tools/ \;
 
 
 cd ../static
-install -m664 *.css *.txt *.swf *.png playing.ico 502.html /srv/eqbeats/static
+install -m664 *.css *.txt *.swf *.png *.jpg playing.ico 502.html /srv/eqbeats/static
 for f in *.js; do
     jsmin < $f > /srv/eqbeats/static/$f
 done
