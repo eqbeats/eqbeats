75806d0d2312233374162daf375becb84d54276a
eqbeats team <eqbeats@eqbeats.org>
2012-06-08 20:02:21 +0200

    transcode.sh: make sure video isn't transcoded

--- transcode.sh
+++ transcode.sh
@@ -8,7 +8,7 @@ fi
 BASE="$EQBEATS_DIR/tracks/$1"
 find "$EQBEATS_DIR/tracks" -name "$1.*" -delete
 
-ffmpeg -loglevel quiet -y -i "$2" -acodec libvorbis -b:a 128k "$BASE.ogg" 2>&1 >> "$EQBEATS_DIR/ffmpeg.log"
+ffmpeg -loglevel quiet -y -i "$2" -acodec libvorbis -b:a 128k -vn "$BASE.ogg" 2>&1 >> "$EQBEATS_DIR/ffmpeg.log"
 
 [ $? -ne 0 ] && exit 1
 
@@ -17,7 +17,7 @@ then
     mv "$2" "$BASE.mp3"
     ln -s "$1.mp3" "$BASE.orig.mp3"
 else
-    ffmpeg -loglevel quiet -y -i "$2" -acodec libmp3lame -q:a 0 "$BASE.mp3" 2>&1 >> "$EQBEATS_DIR/ffmpeg.log"
+    ffmpeg -loglevel quiet -y -i "$2" -acodec libmp3lame -q:a 0 -vn "$BASE.mp3" 2>&1 >> "$EQBEATS_DIR/ffmpeg.log"
     mv "$2" "$BASE.orig.${2##*.}"
 fi
 
