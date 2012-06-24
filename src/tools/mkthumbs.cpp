c070ade5b54a054d082ca0248500b2e0640afc7d
eqbeats team <eqbeats@eqbeats.org>
2012-06-24 08:19:31 +0200

    initialize ImageMagick

--- src/tools/mkthumbs.cpp
+++ src/tools/mkthumbs.cpp
@@ -1,12 +1,14 @@
 #include "../number.h"
 #include "../art.h"
 #include <iostream>
+#include <Magick++.h>
 
 int main(int argc, char **argv){
     if(argc != 2){
         std::cerr << "Usage: mkthumbs ID" << std::endl;
         return 1;
     }
+    Magick::InitializeMagick(NULL);
     int tid = number(argv[1]);
     if(!tid) return 1;
     Art art(tid);
