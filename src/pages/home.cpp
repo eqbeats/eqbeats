f23816432c73374743219cbfb4f121af2aeada75
eqbeats team <eqbeats@eqbeats.org>
2012-07-10 19:30:57 +0200

    separate home and static pages

--- src/pages/home.cpp
+++ src/pages/home.cpp
@@ -2,17 +2,6 @@
 #include <core/cgi.h>
 #include <track/list.h>
 
-void Pages::statics(Document *doc){
-    if(path == "/quickstart")
-        doc->setHtml("html/quickstart.tpl", "Thanks");
-    else if(path == "/faq")
-        doc->setHtml("html/faq.tpl", "FAQ");
-    else if(path == "/credits")
-        doc->setHtml("html/credits.tpl", "Credits");
-    else if(path == "/api")
-        doc->setHtml("html/api.tpl", "API");
-}
-
 void Pages::home(Document *doc){
     if(path != "") return;
     doc->setHtml("html/home.tpl");
