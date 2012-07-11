19da2ea42df146f6985eb083aa63d51e8be48c6d
eqbeats team <eqbeats@eqbeats.org>
2012-07-11 20:23:03 +0200

    favorite tracks: json

--- src/social/pages/favorites.cpp
+++ src/social/pages/favorites.cpp
@@ -5,17 +5,30 @@
 #include <track/list.h>
 
 void Pages::favorites(Document *doc){
-
     std::string sub;
     int uid = route("user", path, sub);
+    if(!uid) return;
 
-    if(!uid || sub != "favorites") return;
+    if(sub == "favorites"){
+        User u(uid);
+        if(!u) return;
+        doc->setHtml("html/tracklist-page.tpl", u.name + " - Favorite tracks");
+        doc->dict()->SetValue("TITLE", u.name + "'s favorite tracks");
+        Follower(uid).favorites().fill(doc->dict(), "TRACKLIST");
+    }
 
-    User u(uid);
-    if(!u) return;
-
-    doc->setHtml("html/tracklist-page.tpl", u.name + " - Favorite tracks");
-    doc->dict()->SetValue("TITLE", u.name + "'s favorite tracks");
-    Follower(uid).favorites().fill(doc->dict(), "TRACKLIST");
+    else if(sub == "favorites/json"){
+        User u(uid);
+        if(!u) return;
+        TrackList tracks = Follower(u.id).favorites();
+        doc->setJson("json/array.tpl");
+        Dict *item, *data;
+        for(TrackList::const_iterator i=tracks.begin(); i!=tracks.end(); i++){
+            item = doc->dict()->AddSectionDictionary("ITEM");
+            data = item->AddIncludeDictionary("DATA");
+            data->SetFilename("json/track.tpl");
+            i->fill(data);
+        }
+    }
 
 }
