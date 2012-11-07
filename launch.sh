812238ad22a7ebd0f53df82dc25bcc5f1fdb7297
Corentin Delcourt <codl@codl.fr>
2012-11-07 08:28:18 +0100

    move transcode.sh to tools, set PATH in launch.sh

--- launch.sh
+++ launch.sh
@@ -13,6 +13,7 @@ function start()
     if [[ $SERVRUNNING -ge $SERVCOUNT ]]; then
       echo "$PROGNAME : Already running"
     else
+      export PATH="$PATH:$EQBEATS_DIR/tools"
       spawn-fcgi -F $SERVCOUNT -P pid -f ./$BINARY -s $SOCKET -M 0775 > /dev/null
       chgrp http eqbeats.sock
       echo "$PROGNAME : Started $SERVCOUNT."
@@ -32,11 +33,11 @@ function stop()
 
 case "$1" in
   start)
-	start
-	;;
+    start
+    ;;
   stop)
-	stop
-	;;
+    stop
+    ;;
   restart)
     stop
     sleep 0.5 # wait for the socket to close properly
