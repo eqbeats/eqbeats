930e88fd3f8f81ef8913cb5d970b0733e70708b5
Corentin Delcourt <codl@codl.fr>
2013-01-13 15:05:55 +0100

    fix console.log in d3.js

--- static/d3.js
+++ static/d3.js
@@ -1644,8 +1644,6 @@
   d3_selectionPrototype.html = function(value) {
     return arguments.length ? this.each(typeof value === "function" ? function() {
       var v = value.apply(this, arguments);
-      console.log(this);
-      console.log(v);
       this.innerHTML = v == null ? "" : v;
     } : value == null ? function() {
       this.innerHTML = "";
