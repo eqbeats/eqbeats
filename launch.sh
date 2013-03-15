a4b94bcf2386e0e7df58190f89b96d82e4230a15
Corentin Delcourt <codl@codl.fr>
2013-03-15 11:36:21 +0100

    launch.sh: don't rely on pidfile
    allows for as much forking as you could ever wish for

--- launch.sh
+++ launch.sh
@@ -9,12 +9,12 @@ cd $EQBEATS_DIR
 
 function start()
 {
-    SERVRUNNING=$(ls /proc | egrep -c "^($(cat pid 2> /dev/null | tr "\n" "|"))$")
+    SERVRUNNING=$(pgrep -c -u $USER "^eqbeats.fcgi$")
     if [[ $SERVRUNNING -ge $SERVCOUNT ]]; then
       echo "$PROGNAME : Already running"
     else
       export PATH="$PATH:$EQBEATS_DIR/tools"
-      spawn-fcgi -F $SERVCOUNT -P pid -f ./$BINARY -s $SOCKET -M 0775 > /dev/null
+      spawn-fcgi -F $SERVCOUNT -f ./$BINARY -s $SOCKET -M 0775 > /dev/null
       chgrp http eqbeats.sock
       echo "$PROGNAME : Started $SERVCOUNT."
     fi
@@ -22,19 +22,14 @@ function start()
 
 function stop()
 {
-    if [[ ! -a pid ]]; then
-      echo "$PROGNAME : Not running."
-    else
-      echo -n   "$PROGNAME : Stopping gracefully..."
-      kill $(cat pid) > /dev/null 2>&1
-      SERVRUNNING=1;
-      while [[ $SERVRUNNING -gt 0 ]]; do
-        sleep 0.1
-        SERVRUNNING=$(ls /proc | egrep -c "^($(cat pid 2> /dev/null | tr "\n" "|"))$")
-      done
-      rm pid
-      echo -e "\r$PROGNAME : Stopped.              "
-    fi
+    echo -n   "$PROGNAME : Stopping gracefully..."
+    pkill -u $USER "^eqbeats.fcgi$" > /dev/null 2>&1
+    SERVRUNNING=1;
+    while [[ $SERVRUNNING -gt 0 ]]; do
+      sleep 0.1
+      SERVRUNNING=$(pgrep -c -u $USER "^eqbeats.fcgi$")
+    done
+    echo -e "\r$PROGNAME : Stopped.              "
 }
 
 case "$1" in
