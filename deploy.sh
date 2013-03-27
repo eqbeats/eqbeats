2123f32e25c9c77acc03fc876af7dd8676de182e
Corentin Delcourt <codl@codl.fr>
2013-03-27 11:21:44 +0100

    update deploy.sh to include rc scripts

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,15 @@
 
 cd /home/eqbeats/eqbeats
 make || return 1
-mkdir -p /srv/eqbeats/{tools,static,templates/{html,json,oembed}}
+mkdir -p /srv/eqbeats/{tools,static,templates/{html,json,oembed},rc}
 
 install -m775 eqbeats.fcgi launch.sh failsafe hitsd.sh ytmgr.sh /srv/eqbeats/
 
 cd tools
 find . -maxdepth 1 -type f -executable -exec install -m775 '{}' /srv/eqbeats/tools/ \;
+
+cd rc
+find . -maxdepth 1 -type f -executable -exec install -m775 '{}' /srv/eqbeats/rc/ \;
 
 cd ../templates
 find . -type f -exec install -m664 '{}' /srv/eqbeats/templates/'{}' \;
