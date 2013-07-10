6f1c99fba206e3dea3134d02de56d3e47757cd55
Frédéric Mangano <fmang@eqbeats.org>
2013-07-10 20:38:53 +0200

    ytmgr: missing parenthesis (sequel of fix argv)

--- src/tools/ytmgr.py
+++ src/tools/ytmgr.py
@@ -174,7 +174,7 @@ Content-Transfer-Encoding: binary
                 if self.auth(sys.argv[2], int(sys.argv[3])) == False:
                     exit(1)
             elif sys.argv[1] == "upload":
-                self.upload(int(sys.argv[2])
+                self.upload(int(sys.argv[2]))
             else:
                 exit(1)
         except TypeError: # silly weak typing
