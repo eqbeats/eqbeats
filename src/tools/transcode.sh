4e8503c4096e2cf1af7b1d89eba03cde6de17037
Corentin Delcourt <codl@codl.fr>
2013-12-08 09:56:03 +0100

    add -nostdin back to ffmpeg commandline
    when -nostdin is not present and ffmpeg is run without a stdin, some
    kind of overflow happens and ffmpeg reads garbage as its stdin, causing
    all sorts of problems
    
    try it:
        $ transcode.sh 1 test.wav &

--- src/tools/transcode.sh
+++ src/tools/transcode.sh
@@ -8,23 +8,23 @@ fi
 BASE="$EQBEATS_DIR/tracks/$1"
 find "$EQBEATS_DIR/tracks" -name "$1.*" -delete
 
-FFMPEG='ffmpeg -loglevel quiet -probesize 10000000'
+FFMPEG='ffmpeg -loglevel quiet -nostdin -probesize 10000000 -vn'
 
-$FFMPEG -i "$2" -acodec libvorbis -q:a 4 -vn "$BASE.ogg"
+$FFMPEG -i "$2" -acodec libvorbis -q:a 4 "$BASE.ogg"
 
 [ $? -ne 0 ] && rm -f "$BASE.ogg" && exit 1
 
 errors=0
 
-$FFMPEG -i "$2" -acodec libfdk_aac -vbr 3 -vn "$BASE.m4a" || ((errors++))
-$FFMPEG -i "$2" -acodec libopus -b:a 128k -vn "$BASE.opus" || ((errors++))
+$FFMPEG -i "$2" -acodec libfdk_aac -vbr 3 "$BASE.m4a" || ((errors++))
+$FFMPEG -i "$2" -acodec libopus -b:a 128k "$BASE.opus" || ((errors++))
 
 if [[ $(file -b --mime-type "$2") = "audio/mpeg" ]]
 then
     mv "$2" "$BASE.mp3"
     ln -s "$1.mp3" "$BASE.orig.mp3"
 else
-    $FFMPEG -i "$2" -acodec libmp3lame -q:a 0 -vn "$BASE.mp3" || ((errors++))
+    $FFMPEG -i "$2" -acodec libmp3lame -q:a 0 "$BASE.mp3" || ((errors++))
     mv "$2" "$BASE.orig.${2##*.}"
 fi
 
