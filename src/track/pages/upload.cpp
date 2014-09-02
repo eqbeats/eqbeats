5d9111c483887602748ea7eaac4a97784432bd02
Frédéric Mangano <fmang@eqbeats.org>
2014-09-02 09:46:01 +0200

    missing slash when exec'ing transcode.sh
    since transcode.sh uses its $0 to find updatetags, it could explain
    why tagging on upload isn't working

--- src/track/pages/upload.cpp
+++ src/track/pages/upload.cpp
@@ -85,7 +85,7 @@ void Pages::trackUpload(Document *doc){
     pid_t worker;
     if(!(worker = fork())){
         freopen("/dev/null","r",stdin);
-        execl(LIBEXEC_DIR "/transcode.sh", LIBEXEC_DIR "transcode.sh", number(t.id).c_str(), tmpFile, NULL);
+        execl(LIBEXEC_DIR "/transcode.sh", LIBEXEC_DIR "/transcode.sh", number(t.id).c_str(), tmpFile, NULL);
         exit(127);
     }
     else{
