5bb90dc4bf6ee824ab27c8474425a9a44c31ce3e
Corentin Delcourt <codl@codl.fr>
2014-11-22 19:41:35 +0100

    transcode.sh: make sure to only take audio
    apparently -vn is kind of broken, also with only -vn we could still grab
    a subtitle track if there had been one in the source file

--- src/tools/transcode.sh
+++ src/tools/transcode.sh
@@ -8,23 +8,23 @@ fi
 BASE="$EQBEATS_DIR/tracks/$1"
 find "$EQBEATS_DIR/tracks" -name "$1.*" -delete
 
-FFMPEG='ffmpeg -loglevel quiet -nostdin -probesize 10000000 -vn'
+FFMPEG="ffmpeg -loglevel quiet -nostdin -probesize 10000000 -i $2 -map 0:a"
 
-$FFMPEG -i "$2" -acodec libvorbis -q:a 4 "$BASE.ogg"
+$FFMPEG -acodec libvorbis -q:a 4 "$BASE.ogg"
 
 [ $? -ne 0 ] && rm -f "$BASE.ogg" && exit 1
 
 errors=0
 
-$FFMPEG -i "$2" -acodec libfdk_aac -vbr 3 "$BASE.m4a" || ((errors++))
-$FFMPEG -i "$2" -acodec libopus -b:a 128k "$BASE.opus" || ((errors++))
+$FFMPEG -acodec libfdk_aac -vbr 3 "$BASE.m4a" || ((errors++))
+$FFMPEG -acodec libopus -b:a 128k "$BASE.opus" || ((errors++))
 
 if [[ $(file -b --mime-type "$2") = "audio/mpeg" ]]
 then
     mv "$2" "$BASE.mp3"
     ln -s "$1.mp3" "$BASE.orig.mp3"
 else
-    $FFMPEG -i "$2" -acodec libmp3lame -q:a 0 "$BASE.mp3" || ((errors++))
+    $FFMPEG -acodec libmp3lame -q:a 0 "$BASE.mp3" || ((errors++))
     mv "$2" "$BASE.orig.${2##*.}"
 fi
 
