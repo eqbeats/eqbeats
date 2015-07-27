f62716748d6e103a1fe5c4c8c67b4856adb60af5
codl (Corentin Delcourt) <codl@codl.fr>
2015-07-27 10:07:00 +0200

    make updatetags use Audio::updateTags(), hopefully fixing issue #25
    thanks fmang for figuring out that linking stuff for me

--- src/tools/updatetags.cpp
+++ src/tools/updatetags.cpp
@@ -1,26 +1,12 @@
+#include <eqbeats.h>
 #include <core/db.h>
-#include <core/path.h>
+#include <track/audio.h>
 #include <text/text.h>
 #include <iostream>
-#include <taglib/mpegfile.h>
-#include <taglib/vorbisfile.h>
-#include <taglib/mp4file.h>
-#include <taglib/opusfile.h>
-#include <taglib/xiphcomment.h>
-#include <taglib/taglib.h>
-#include <taglib/tpropertymap.h>
 
 using namespace std;
-using namespace TagLib;
 
 int main(int argc, char **argv){
-
-    const char *dir = getenv("EQBEATS_DIR");
-    if(!dir){
-        cerr << "EQBEATS_DIR not set." << endl;
-        return 1;
-    }
-
     if(argc != 2){
         cerr << "Usage: updatetags ID" << endl;
         return 1;
@@ -32,63 +18,24 @@ int main(int argc, char **argv){
         return 1;
     }
 
-    DB::connect("updatetags");
-
-    DB::Result r = DB::query("SELECT tracks.id, tracks.title, users.name FROM tracks, users WHERE users.id = tracks.user_id AND tracks.id = $1", number(id));
-
-    for(DB::Result::const_iterator i=r.begin(); i!=r.end(); i++){
-        cout << (*i)[0] << " " << (*i)[1] << endl;
-        String t((*i)[1], String::UTF8), a((*i)[2], String::UTF8);
-        // MP3
-        std::string mp3path = (string) dir + "/tracks/" + (*i)[0] + ".mp3";
-        MPEG::File mp3(mp3path.c_str());
-        Tag *tag = mp3.tag();
-        tag->setTitle(t);
-        tag->setArtist(a);
-        mp3.save(MPEG::File::ID3v1 | MPEG::File::ID3v2);
-        // Vorbis
-        std::string oggpath = (string) dir + "/tracks/" + (*i)[0] + ".ogg";
-        Ogg::Vorbis::File ogg(oggpath.c_str());
-        Tag *oggtag = ogg.tag();
-        if(oggtag != NULL){
-            oggtag->setTitle(t);
-            oggtag->setArtist(a);
-        } else {
-            Ogg::XiphComment comment;
-            comment.setTitle(t);
-            comment.setArtist(a);
-            ogg.setProperties(comment.properties());
-        }
-        ogg.save();
-        // AAC
-        std::string aacpath = (string) dir + "/tracks/" + (*i)[0] + ".m4a";
-        MP4::File aac(aacpath.c_str());
-        TagLib::Tag *aactag = aac.tag();
-        if(aactag != NULL){
-            aactag->setTitle(t);
-            aactag->setArtist(a);
-        } else {
-            PropertyMap map;
-            map.insert("TITLE", t);
-            map.insert("ARTIST", a);
-            aac.setProperties(map);
-        }
-        aac.save();
-        // Opus
-        std::string opuspath = (string) dir + "/tracks/" + (*i)[0] + ".opus";
-        Ogg::Opus::File opus(opuspath.c_str());
-        Tag *opustag = opus.tag();
-        if(opustag != NULL){
-            opustag->setTitle(t);
-            opustag->setArtist(a);
-        } else {
-            PropertyMap map;
-            map.insert("TITLE", t);
-            map.insert("ARTIST", a);
-            opus.setProperties(map);
-        }
-        opus.save();
+    struct eqbeats_ctx eq;
+    if(eqbeats_init(&eq, "", NULL, 0) != 0){
+        cerr << "eqbeats initialization failed, check that the EQBEATS_POSTGRES environment variable is correct" << endl;
+        return 1;
     }
+
+    DB::connect("updatetags");
+    DB::setPgDatabase(eq.pg);
+
+    Track t(id);
+
+    if (!t){
+        cerr << "track does not exist" << endl;
+        return 1;
+    }
+
+    Audio a(&t);
+    a.updateTags();
 
     DB::close();
     return 0;
